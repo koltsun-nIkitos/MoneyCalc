0eaeac8d25d1b6bd349ca70ed5fc9e221042cb14
Koltsun Nikita <koltsun-nv@yandex.ru>
2023-09-06 21:37:08 +0300

    v1.0.0

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-const Header = ({ text }) => <h1 id ="hi">{text}</h1>
-
-const Element = <p>Параграф</p>
-
-class Main extends React.Component{
-  render() {
-    return (
-      <div className='main'>
-        <Header text={'Свойство текст'}/>
-        <Element />
-      </div>
-    )
-  }
-}
+import './normalize.css';
+import './style.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Main />
-
+    <App />
   </React.StrictMode>
 );
 
